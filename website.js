4a6a67790e1dfb6fe7e351510362da91fee22bb4
maddyjaime <40634442+maddyjaime@users.noreply.github.com>
2018-06-27 09:49:05 -0400

    Add files via upload

---
+++ website.js
@@ -0,0 +1,9 @@
+function changeme() {
+  if (document.getElementById('image1').src=="https://i.pinimg.com/736x/04/ee/09/04ee0998b01bf0421ba5fa0eb26d9b44--baby-elephant-drawing-elephant-sketch.jpg")
+  {
+    document.getElementById('image2').src = "https://img.etsystatic.com/il/64aade/236142785/il_570xN.236142785.jpg?version=0";
+  }
+  else{
+    document.getElementById('image1').src = "https://i.pinimg.com/736x/04/ee/09/04ee0998b01bf0421ba5fa0eb26d9b44--baby-elephant-drawing-elephant-sketch.jpg";
+  }
+}
